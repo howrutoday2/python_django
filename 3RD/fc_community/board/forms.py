8f1b3bff6267fd3dd495ef972ea15eb9199e5a41
howrutoday <howrutoday.ys@gmail.com>
2022-04-07 16:05:44 +0900

    게시판

---
+++ 3RD/fc_community/board/forms.py
@@ -0,0 +1,16 @@
+from socket import fromshare
+from django import forms
+# from .models import Fcuser
+from django.contrib.auth.hashers import check_password
+
+class BoardForm(forms.Form):
+    title=forms.CharField(
+        error_messages={
+            'required':'제목을 입력해주세요'
+        },
+        max_length=128, label="제목")
+    contents =forms.CharField(
+        error_messages={
+            'required':'내용을 입력해주세요'
+        },
+        widget=forms.Textarea, label ="내용")
